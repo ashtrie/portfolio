6ff635c5da9d167134548e4c1927885f95472ed9
ashtrie <aleksandersher@gmail.com>
2017-05-08 13:03:52 +0300

    v

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
-}).listen(process.env.PORT || 3000, 'localhost', function (err, result) {
+}).listen(process.env.PORT || 3000, function (err, result) {
     if (err) {
         console.log(err);
     }
